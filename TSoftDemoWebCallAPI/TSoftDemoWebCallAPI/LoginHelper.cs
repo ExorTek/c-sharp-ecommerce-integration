54fe7ee8b91913fdd2da400f19ccd166e6c8a5d4
ExorTek <exortek979@gmail.com>
2021-11-26 15:39:35 +0300

    Create LoginHelper.cs

---
+++ TSoftDemoWebCallAPI/TSoftDemoWebCallAPI/LoginHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace TSoftDemoWebCallAPI
+{
+    public class LoginHelper
+    {
+        public UserDto Login(string userName)
+        {
+            var client = new RestClient("https://mehmetdemirel.1ticaret.com/rest1/auth/login/"+userName);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("pass", "Postman01");
+            request.RequestFormat = DataFormat.Json;
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            var user = JsonConvert.DeserializeObject<UserDto>(content);
+            return user;
+            
+        }
+    }
+}

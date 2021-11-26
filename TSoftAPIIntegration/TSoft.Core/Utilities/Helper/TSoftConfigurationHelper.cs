e67b474201744e608d2da3496d64c44378df8dda
ExorTek <exortek979@gmail.com>
2021-11-26 15:41:20 +0300

    -

---
+++ TSoftAPIIntegration/TSoft.Core/Utilities/Helper/TSoftConfigurationHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace TSoft.Core.Utilities.Helper
+{
+    public class TSoftConfigurationHelper
+    {
+
+        public static string Token = "";
+        public static string LoginUrl = "http://mehmetdemirel.1ticaret.com/rest1/auth/login/";
+        public static string UserName = "postman";
+        public static string Password = "Postman01";
+        public static string ProductUrl = "http://mehmetdemirel.1ticaret.com/rest1/product/get/";
+    }
+}

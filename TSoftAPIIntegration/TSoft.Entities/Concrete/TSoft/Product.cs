a74695e7c7f389929a32d8f82611a1a8bc809c60
ExorTek <exortek979@gmail.com>
2021-11-26 15:41:02 +0300

    -

---
+++ TSoftAPIIntegration/TSoft.Entities/Concrete/TSoft/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using TSoft.Core.Entities;
+
+namespace TSoft.Entities.Concrete.TSoft
+{
+    public class Product : IEntitiy
+    {
+        public bool Success { get; set; }
+        public List<ProductData> Data { get; set; }
+    }
+
+    public class ProductData
+    {
+        public string ProductId { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public string Barcode { get; set; }
+        public string Stock { get; set; }
+        public string SellingPrice { get; set; }
+    }
+
+}
---
+++ TSoftAPIIntegration/TSoft.Entities/Concrete/TSoft/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using TSoft.Core.Entities;
+
+namespace TSoft.Entities.Concrete.TSoft
+{
+    public class User : IEntitiy
+    {
+        public bool Success { get; set; }
+        public List<UserData> Data { get; set; }
+    }
+
+    public  class UserData
+    {
+        public string Username { get; set; }
+        public string Toksen { get; set; }
+        public string SecretKey { get; set; }
+        public string Type { get; set; }
+    }
+
+  
+}

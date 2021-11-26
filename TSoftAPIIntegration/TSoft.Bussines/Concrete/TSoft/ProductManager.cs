ab14808f6be7410afa28f554910c50cb0213e659
ExorTek <exortek979@gmail.com>
2021-11-26 15:42:57 +0300

    update

---
+++ TSoftAPIIntegration/TSoft.Bussines/Concrete/TSoft/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿
+using TSoft.Business.Abstract.TSoft;
+using TSoft.DataAccess.Abstract.TSoft;
+using TSoft.Entities.Concrete.TSoft;
+
+namespace TSoft.Business.Concrete.TSoft
+{
+    public class ProductManager :IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+
+        public Product GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ TSoftAPIIntegration/TSoft.Bussines/Concrete/TSoft/UserManager.cs
@@ -0,0 +1,20 @@
+﻿using TSoft.DataAccess.Abstract.TSoft;
+using TSoft.Entities.Concrete.TSoft;
+
+namespace TSoft.Business.Concrete.TSoft
+{
+    public class UserManager 
+    {
+         IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public User Login()
+        {
+            return _userDal.Login();
+        }
+    }
+}
